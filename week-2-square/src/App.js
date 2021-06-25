e3936a1aa94ca262d8b7a41eba716c3bb72845e1
kyungjoon.park <pkj6921@naver.com>
2021-06-25 23:16:39 +0900

    리액트 - 2강 클래스형 컴포넌트에서 state 관리하는 법 주석 추가

--- week-2-square/src/App.js
+++ week-2-square/src/App.js
@@ -10,12 +10,12 @@ class App extends React.Component {
   }
   
   addNemo = () => {
-    this.setState({count: this.state.count + 1})
+    this.setState({count: this.state.count + 1}) // key는 count, value는 this.state.count
   }
 
   removeNemo = () => {
     if (this.state.count > 0) {
-      this.setState({count: this.state.count - 1})
+      this.setState({count: this.state.count - 1}) // 계속 줄다보면 this.state.count가 0 이하가 되는데 이때는 UX를 고려하여 alert을 날려주자.
     } else {
       alert('더 이상은 naver...!')
     }
@@ -27,7 +27,7 @@ class App extends React.Component {
     
     return (
       <div className="App">
-        {nemo_count.map((num, index) => {
+        {nemo_count.map((num, index) => { // 돔 구조 안에서 map을 이용한 반복문을 사용하기 위해 {} bracket으로 감싸주었음.
           return (
             <div key={index} style={{
               width: '150px',
