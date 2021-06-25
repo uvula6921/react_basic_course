61b5ec35472570af7bafb277cf8ee2f01d7833a2
kyungjoon.park <pkj6921@naver.com>
2021-06-25 23:13:01 +0900

    리액트 - 2강 클래스형 컴포넌트에서 state 관리하는 법

--- week-2-square/src/App.js
+++ week-2-square/src/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import styled from 'styled-components'
 
 class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-
+      count: 3,
     };
+  }
+  
+  addNemo = () => {
+    this.setState({count: this.state.count + 1})
+  }
 
+  removeNemo = () => {
+    if (this.state.count > 0) {
+      this.setState({count: this.state.count - 1})
+    } else {
+      alert('더 이상은 naver...!')
+    }
   }
 
   render() {
-
+    
+    const nemo_count = Array.from({length: this.state.count}, (item, index) => index)
+    
     return (
       <div className="App">
-
+        {nemo_count.map((num, index) => {
+          return (
+            <div key={index} style={{
+              width: '150px',
+              height: '150px',
+              backgroundColor: '#eee',
+              margin: '10px'
+            }}>
+              nemo
+            </div>
+          )
+        })}
+        <button onClick={this.addNemo}>+</button>
+        <button onClick={this.removeNemo}>-</button>
       </div>
     );
   }
