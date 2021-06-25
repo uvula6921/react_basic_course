83712a63985e18b107f413fc5bb813ab6fc469a8
kyungjoon.park <pkj6921@naver.com>
2021-06-25 19:38:20 +0900

    리액트 - 1강 과제 main.js 수정, gitignore 추가

--- week-1-homework/src/Main.js
+++ week-1-homework/src/Main.js
@@ -4,7 +4,7 @@ import Sarang from './sarang.png'
 const Main = ({name}) => {
   return (
     <div className="wrap">
-      <img className="img" src={Sarang} alt="사랑이"></img>
+      <img className="img" src={Sarang} alt={name}></img>
       <div className="container">
           <h1 className="title">나는 <span>{name}</span>에 대해 얼마나 알고 있을까?</h1>
           <input className="inputBox" type="text" placeholder="내 이름"></input>
