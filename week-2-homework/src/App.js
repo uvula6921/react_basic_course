58a57dba51ebfbbec90730c282c0ec8137ee3b6a
kyungjoon.park <pkj6921@naver.com>
2021-06-26 11:24:46 +0900

    리액트 - 2강 숙제 앱 생성

---
+++ week-2-homework/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components'
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+export default App;
